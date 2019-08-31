c08a267a9a17423082415ad4c7de7daa3928e3df
psymin <psymin@runlinux.net>
2019-08-31 21:55:29 +0000

    set up npm env for local user

---
+++ npm-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# set up the npm environment for local user installs
+# And install gamedig
+
+mkdir ~/.node
+
+echo "prefix = ~/.node" >> ~/.npmrc
+echo "PATH=\"\$HOME/.node/bin:\$PATH\"" >> ~/.profile
+echo "NODE_PATH=\"\$HOME/.node/lib/node_modules:\$NODE_PATH\"" >> ~/.profile
+echo "MANPATH=\"\$HOME/.node/share/man:\$MANPATH\"" >> ~/.profile
+
+npm install gamedig -g
